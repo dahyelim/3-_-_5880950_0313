4af84444d03af084907bf0727f1bb8d0993014aa
dahyelim <lim3016566@naver.com>
2025-03-13 15:50:20 +0900

    3완성

--- 3완성_임다혜_5880950_0313/3완성_임다혜_5880950_0313.cpp
+++ 3완성_임다혜_5880950_0313/3완성_임다혜_5880950_0313.cpp
@@ -3,20 +3,25 @@
 #include<string.h>
 
 int main() {
-	char str[100];
-	printf("문자열을 입력하세요 : ");
-	scanf("%[^\n]s", str);
-	int str_len = strlen(str);
-	printf("str_len");
-	for (int i = 0; i < str_len; ++i) {
-		if (str[i] >= 'a' && str[i] <= 'z') {
-			str[i] -= 32;
-		}
-		else if (str[i] >= 'A' && str[i] <= 'Z') {
-			str[i] += 32;
-		}
-	}
-	printf("문자열의 길이 : %d\n", str_len);
-	printf("변환된 문자열 : %s",str);
+    char str[100]; // 문자열을 저장할 배열 선언
 
+    printf("문자열을 입력하세요 : "); 
+    scanf("%[^\n]s", str); // 엔터키 전까지 입력받은 문자열을 str 배열에 저장
+
+    int str_len = strlen(str); // 입력된 문자열의 길이를 구하여 str_len에 저장
+
+    // 입력된 문자열을 대소문자 변환
+    for (int i = 0; i < str_len; ++i) {
+        if (str[i] >= 'a' && str[i] <= 'z') { // 소문자라면
+            str[i] -= 32; // 소문자를 대문자로 변환 (ASCII 값 차이: 'a' - 'A' = 32)
+        }
+        else if (str[i] >= 'A' && str[i] <= 'Z') { // 대문자라면
+            str[i] += 32; // 대문자를 소문자로 변환 (ASCII 값 차이: 'A' - 'a' = -32)
+        }
+    }
+
+    printf("문자열의 길이 : %d\n", str_len); // 변환 후 문자열의 길이를 출력
+    printf("변환된 문자열 : %s", str); // 변환된 문자열을 출력
+
+    return 0; // 프로그램 종료
 }
